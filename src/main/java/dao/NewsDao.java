eaaee912bc45bfa0be7a95382ce59b5946636ea5
dowoo <kimdow6@gmail.com>
2022-06-18 20:37:59 +0900

    6-18 김도우 네비랑 뉴스 연결 article 추가

--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -10,6 +10,7 @@ public class NewsDao {
 
 	private static NewsDao instance = new NewsDao();
 	private NewsDao() {}
+	
 	public static NewsDao getInstance() {
 		return instance;
 	}
@@ -28,19 +29,16 @@ public class NewsDao {
 	
 	public List<News> getNews(int beginIndex, int endIndex) throws SQLException{
 		String sql = "select news_no, club_no, league_no, news_name, news_content, news_title, news_created_date, news_updated_date "
-
-				+ "from (select row_number() over (order by news_no asc) row_number, "
-				+ "			   news_no, club_no, league_no, news_name, news_content, news_title, news_created_date, news_updated_date "
-				+ "		from soccer_news)"
-				+ "where row_number >= ? and row_number <= ? "
-				+ "order by news_no";
+				   + "from (select row_number() over (order by news_no asc) row_number, "
+				   + "			   news_no, club_no, league_no, news_name, news_content, news_title, news_created_date, news_updated_date "
+				   + "		from soccer_news)"
+				   + "where row_number >= ? and row_number <= ? "
+				   + "order by news_no";
 
 
 		return helper.selectList(sql, rs -> {
-
 			News news = new News();
-
-			
+		
 			news.setNewsNo(rs.getInt("NEWS_NO"));
 			news.setNewsName(rs.getString("NEWS_NAME"));
 			news.setClubNo(rs.getInt("CLUB_NO"));
@@ -55,5 +53,70 @@ public class NewsDao {
 		},beginIndex, endIndex);			
 	}
 	
+	public News getArticleByNO(int newsNo) throws SQLException{
+		String sql = "select * "
+				   + "from soccer_news "
+				   + "where news_no = ? "
+				   + "order by news_no ";
+
+		return helper.selectOne(sql, rs -> {
+			News news = new News();
+			
+			news.setNewsNo(rs.getInt("NEWS_NO"));
+			news.setNewsName(rs.getString("NEWS_NAME"));
+			news.setClubNo(rs.getInt("CLUB_NO"));
+			news.setLeagueNo(rs.getInt("LEAGUE_NO"));
+			news.setNewsCreatedDate(rs.getDate("NEWS_CREATED_DATE"));
+			news.setNewsUpdatedDate(rs.getDate("NEWS_UPDATED_DATE"));
+			news.setNewsTitle(rs.getString("NEWS_TITLE"));
+			news.setNewsContent(rs.getString("NEWS_CONTENT"));
+				
+			
+			return news;
+		},newsNo);
+	}
 	
+	public List<News> getLeagueNoNews(int leagueNo) throws SQLException{
+		String sql = "select * "
+				   + "from soccer_news "
+				   + "where news_no = ? ";
+
+		return helper.selectList(sql, rs -> {
+			News news = new News();
+			
+			news.setNewsNo(rs.getInt("NEWS_NO"));
+			news.setNewsName(rs.getString("NEWS_NAME"));
+			news.setClubNo(rs.getInt("CLUB_NO"));
+			news.setLeagueNo(rs.getInt("LEAGUE_NO"));
+			news.setNewsCreatedDate(rs.getDate("NEWS_CREATED_DATE"));
+			news.setNewsUpdatedDate(rs.getDate("NEWS_UPDATED_DATE"));
+			news.setNewsTitle(rs.getString("NEWS_TITLE"));
+			news.setNewsContent(rs.getString("NEWS_CONTENT"));
+				
+			
+			return news;
+		},leagueNo);
+	}
+	
+	public List<News> getArticleNByNews(int newsNo) throws SQLException{
+		String sql = "select * "
+				   + "from soccer_news "
+				   + "where news_no = ? ";
+		
+		return helper.selectList(sql, rs -> {
+			News news = new News();
+			
+			news.setNewsNo(rs.getInt("NEWS_NO"));
+			news.setNewsName(rs.getString("NEWS_NAME"));
+			news.setClubNo(rs.getInt("CLUB_NO"));
+			news.setLeagueNo(rs.getInt("LEAGUE_NO"));
+			news.setNewsCreatedDate(rs.getDate("NEWS_CREATED_DATE"));
+			news.setNewsUpdatedDate(rs.getDate("NEWS_UPDATED_DATE"));
+			news.setNewsTitle(rs.getString("NEWS_TITLE"));
+			news.setNewsContent(rs.getString("NEWS_CONTENT"));
+			
+			
+			return news;
+		},newsNo);
+	}
 }
